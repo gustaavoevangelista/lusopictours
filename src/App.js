d8be2a61cb2ede07109610ddae1d9bf1a28f5dc7
gustaavoevangelista <gustavo.eng.henrique@gmail.com>
2022-11-18 16:07:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,453 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="desktop-1">
+      <div className="footer-div">
+        <div className="frame-div" />
+        <div className="frame-div1">
+          <div className="selecione-um-pais">Selecione um pais</div>
+          <div className="frame-div2">
+            <img
+              className="icoutline-greater-than-icon"
+              alt=""
+              src="img/icon-greater-than.svg"
+            />
+            <img
+              className="twemojiflag-portugal-icon"
+              alt=""
+              src="/img/icon-flag-portugal.svg"
+            />
+            <div className="portugal-div">Portugal</div>
+          </div>
+        </div>
+        <div className="contactar-condicoes-legais-pol-div">
+          <span className="contactar-condicoes-legais-pol-span">
+            <p className="contactar-p">Contactar</p>
+            <p className="contactar-p">Condicoes legais</p>
+            <p className="contactar-p">Politica de privacidade</p>
+            <p className="contactar-p">Politica de cookies</p>
+            <p className="contactar-p">Gerir preferencias</p>
+            <p className="contactar-p">Registo empresas</p>
+            <p className="contactar-p">Quem somos?</p>
+            <p className="contactar-p">Equipa Editorial</p>
+            <p className="careers-p">Careers</p>
+          </span>
+        </div>
+        <div className="informacoes-div">Informações</div>
+        <div className="siga-nos-em-div">Siga-nos em</div>
+        <div className="social-icons-div">
+          <img
+            className="akar-iconsfacebook-fill"
+            alt=""
+            src="/img/icons-facebook.svg"
+          />
+          <img
+            className="akar-iconsfacebook-fill"
+            alt=""
+            src="/img/icons-twitter.svg"
+          />
+          <img
+            className="akar-iconsfacebook-fill"
+            alt=""
+            src="/img/icons-pinterest.svg"
+          />
+          <img
+            className="akar-iconsfacebook-fill"
+            alt=""
+            src="/img/icons-instagram.svg"
+          />
+        </div>
+      </div>
+
+      <div className="testimonials-div">
+        <div className="frame-div3">
+          <div className="the-point-of-using-lorem-ipsum">
+            The point of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters.
+          </div>
+        </div>
+        <img className="image-2-icon" alt="" src="../image-2@2x.png" />
+        <div className="frame-div4">
+          <div className="many-desktop-publishing-packag">
+            Many desktop publishing packages and web page editors now use Lorem
+            Ipsum as their default model text, and a search for
+          </div>
+        </div>
+        <img className="image2-1-icon" alt="" src="../image2-1@2x.png" />
+        <div className="frame-div5" />
+        <img className="image1-1-icon" alt="" src="../image1-1@2x.png" />
+        <div className="it-is-a-long-established-fact">
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout.
+        </div>
+        <div className="veja-o-top10">
+          <div className="ver-todos-os-testimonials">
+            Ver todos os testimonials 
+          </div>
+          <div className="biarrow-down-short-div" />
+          <img className="vector-icon" alt="" src="../vector.svg" />
+        </div>
+        <div className="frame-div6">
+          <div className="div">4,8</div>
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+        </div>
+        <div className="frame-div7">
+          <div className="div">4,5</div>
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+        </div>
+        <div className="frame-div8">
+          <div className="div">4,9</div>
+          <img className="vector-icon1" alt=""src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt=""src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+          <img className="vector-icon1" alt="" src="/img/icon-star.svg" />
+        </div>
+      </div>
+      <div className="clients-testimonials-div">
+        <span className="contactar-condicoes-legais-pol-span">
+          <p className="contactar-p">
+            <span>Clients</span>
+          </p>
+          <p className="testimonials-p">
+            <span>Testimonials</span>
+          </p>
+        </span>
+      </div>
+      <div className="nossos-servios-div">
+        <div className="cadastre-e-aluge-casa">
+          <img
+            className="fluent-emojihouse-icon"
+            alt=""
+            src="../fluentemojihouse.svg"
+          />
+          <div className="services-button-div">
+            <div className="cadastre-e-alugue-j-o-seu-apa">
+              Cadastre e alugue já o seu apartamento
+            </div>
+          </div>
+        </div>
+        <div className="pacotes-turisticos-div">
+          <img
+            className="fluent-emojihouse-icon"
+            alt=""
+            src="../flatcoloriconsbinoculars.svg"
+          />
+          <div className="services-button-div">
+            <div className="cadastre-e-alugue-j-o-seu-apa">
+              Pacotes turísticos
+            </div>
+          </div>
+        </div>
+        <div className="ensaios-fotograficos-div">
+          <img
+            className="fluent-emojihouse-icon"
+            alt=""
+            src="../emojionev1camerawithflash.svg"
+          />
+          <div className="services-button-div">
+            <div className="ensaios-fotogrficos-div">Ensaios fotográficos</div>
+          </div>
+        </div>
+      </div>
+      <div className="top-destinos-europa">
+        <div className="top-1-destino-da-europa">
+          <div className="frame-div11">
+            <div className="liubliania-eslovnia-div">
+              1 - Liubliania, Eslovênia
+            </div>
+          </div>
+          <img
+            className="liubliana-eslovenia-1-icon"
+            alt=""
+            src="../liublianaeslovenia-1@2x.png"
+          />
+        </div>
+        <div className="top-3-destino-da-europa">
+          <div className="frame-div11">
+            <div className="liubliania-eslovnia-div">3 - Amiens, França</div>
+          </div>
+          <img
+            className="liubliana-eslovenia-1-icon"
+            alt=""
+            src="../amiensfranca-1@2x.png"
+          />
+        </div>
+        <div className="top-2-destino-da-europa">
+          <div className="frame-div11">
+            <div className="liubliania-eslovnia-div">2 - Marbella, Espanha</div>
+          </div>
+          <img
+            className="liubliana-eslovenia-1-icon"
+            alt=""
+            src="../marbellaespanha-1@2x.png"
+          />
+        </div>
+        <div className="veja-o-top101">
+          <div className="veja-o-top-10">Veja o Top 10</div>
+          <img
+            className="biarrow-down-short-icon"
+            alt=""
+            src="../biarrowdownshort.svg"
+          />
+        </div>
+      </div>
+      <div className="top-destinos-europa-2022">
+        <span className="contactar-condicoes-legais-pol-span">
+          <span>{`Top destinos `}</span>
+          <span className="span">{` `}</span>
+          <span>{` `}</span>
+          <span className="europa-2022-span">Europa 2022</span>
+        </span>
+      </div>
+      <div className="carousel-div">
+        <div className="slider-div">
+          <div className="card06-div">
+            <div className="info-div">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <div className="img-div">
+              <img className="ap1-1-icon" alt="" src="../ap1-1@2x.png" />
+              <div className="frame-div14">
+                <div className="premium-div">Premium</div>
+              </div>
+            </div>
+          </div>
+          <div className="card03-div">
+            <div className="info-div1">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <div className="img-div">
+              <img className="ap1-1-icon" alt="" src="../ap3-1@2x.png" />
+              <div className="frame-div15">
+                <div className="premium-div">Premium</div>
+              </div>
+            </div>
+          </div>
+          <div className="card10-div">
+            <div className="info-div1">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <div className="img-div">
+              <img className="ap1-1-icon" alt="" src="../ap4-1@2x.png" />
+              <div className="frame-div16">
+                <div className="premium-div">Premium</div>
+              </div>
+            </div>
+          </div>
+          <div className="card02-div">
+            <div className="info-div1">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <img className="ap2-1-icon" alt="" src="../ap2-1@2x.png" />
+            <div className="frame-div17">
+              <div className="premium-div">Premium</div>
+            </div>
+          </div>
+          <div className="card09-div">
+            <div className="info-div1">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <div className="img-div">
+              <img className="ap1-1-icon" alt="" src="../ap4-1@2x.png" />
+              <div className="frame-div14">
+                <div className="premium-div">Premium</div>
+              </div>
+            </div>
+          </div>
+          <div className="card08-div">
+            <div className="info-div1">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <div className="img-div">
+              <img className="ap1-1-icon" alt="" src="../ap4-1@2x.png" />
+              <div className="frame-div14">
+                <div className="premium-div">Premium</div>
+              </div>
+            </div>
+          </div>
+          <div className="card07-div">
+            <div className="info-div1">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <div className="img-div">
+              <img className="ap1-1-icon" alt="" src="../ap4-1@2x.png" />
+              <div className="frame-div14">
+                <div className="premium-div">Premium</div>
+              </div>
+            </div>
+          </div>
+          <div className="card04-div">
+            <div className="info-div1">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <div className="img-div">
+              <img className="ap1-1-icon" alt="" src="../ap4-1@2x.png" />
+              <div className="frame-div16">
+                <div className="premium-div">Premium</div>
+              </div>
+            </div>
+          </div>
+          <div className="card05-div">
+            <div className="info-div1">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <div className="img-div">
+              <img className="ap1-1-icon" alt="" src="../ap1-14@2x.png" />
+              <div className="frame-div14">
+                <div className="premium-div">Premium</div>
+              </div>
+            </div>
+          </div>
+          <div className="card01-div">
+            <div className="info-div1">
+              <div className="title-rating-price">
+                <span className="contactar-condicoes-legais-pol-span">
+                  <p className="contactar-p">Title</p>
+                  <p className="contactar-p">Rating</p>
+                  <p className="careers-p">Price</p>
+                </span>
+              </div>
+            </div>
+            <div className="img-div">
+              <img className="ap1-1-icon" alt="" src="../ap1-14@2x.png" />
+              <div className="frame-div14">
+                <div className="premium-div">Premium</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="button-right-div">
+          <div className="div3">{`>`}</div>
+        </div>
+        <img className="dots-icon" alt="" src="../dots.svg" />
+      </div>
+      <div className="section-div">
+        <img
+          className="background-mundi-map"
+          alt=""
+          src="../background-mundi-map@2x.png"
+        />
+        <div className="img-div9">
+          <img
+            className="img-1section-1-icon"
+            alt=""
+            src="../img1section-1@2x.png"
+          />
+          <b className="pic-tours-b">
+            <span className="contactar-condicoes-legais-pol-span">
+              <p className="contactar-p">PIC</p>
+              <p className="careers-p">TOURS</p>
+            </span>
+          </b>
+          <img
+            className="evamenu-outline-icon"
+            alt=""
+            src="../evamenuoutline.svg"
+          />
+          <img className="mdimagnify-icon" alt="" src="../mdimagnify.svg" />
+        </div>
+        <div className="search-bar-div">
+          <div className="frame-div24">
+            <b className="search-b">Search</b>
+          </div>
+          <div className="frame-div25">
+            <b className="datas-b">datas</b>
+          </div>
+          <div className="frame-div26">
+            <b className="local-b">local</b>
+          </div>
+        </div>
+        <b className="lu-so-b">
+          <span className="contactar-condicoes-legais-pol-span">
+            <p className="contactar-p">LU</p>
+            <p className="careers-p">SO</p>
+          </span>
+        </b>
+        <img
+          className="logo-lusopictours-icon"
+          alt=""
+          src="../logolusopictours@2x.png"
+        />
+      </div>
+      <div className="nossos-servios-div1">
+        <span className="contactar-condicoes-legais-pol-span">
+          <p className="contactar-p">
+            <span className="nossos-span">
+              <span>Nossos</span>
+            </span>
+          </p>
+          <p className="servicos-p">
+            <span className="servicos-span">Servicos</span>
+          </p>
+        </span>
+      </div>
     </div>
+    </>
   );
 }
 
